a7598a2b5372685475c3d41c60a8ab27de6f0815
Felipe Coury <felipe.coury@gmail.com>
2021-03-17 15:44:57 -0300

    fix: upgraded pg

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ async function connect() {
   });
 }
 
-async function exec(client, sql, values=[]) {
+async function exec(client, sql, values = []) {
   return new Promise((resolve, reject) => {
     client.query(sql, values, (err, result) => {
       if (err) { return reject(err); }
@@ -48,40 +48,47 @@ async function checkTable(client) {
 }
 
 async function run() {
-  const client = await connect();
-  const feed = await parser.parseURL(process.env.FEED_URL);
-  const forums = process.env.FORUMS && process.env.FORUMS.split(',');
-  const webhook = process.env.WEBHOOK_URL;
+  try {
+    console.log('connecting');
+    const client = await connect();
+    console.log('getting feed');
+    const feed = await parser.parseURL(process.env.FEED_URL);
+    const forums = process.env.FORUMS && process.env.FORUMS.split(',');
+    const webhook = process.env.WEBHOOK_URL;
 
-  await checkTable(client);
+    await checkTable(client);
 
-  await Promise.all(feed.items.map(async item => {
-    const { title, categories, link } = item;
-    const category = categories[0];
-    const topic = link.split('?topic=')[1].split('.')[0];
-    if (!title.startsWith('Re:')) {
-      if (!forums || forums.indexOf(category)) {
-        if (await isNotified(client, topic)) {
-          console.log('Already notified -', topic, title);
-          return;
+    await Promise.all(feed.items.map(async item => {
+      const { title, categories, link } = item;
+      const category = categories[0];
+      const topic = link.split('?topic=')[1].split('.')[0];
+      if (!title.startsWith('Re:')) {
+        if (!forums || forums.indexOf(category)) {
+          if (await isNotified(client, topic)) {
+            console.log('Already notified -', topic, title);
+            return;
+          }
+
+          console.log('Notifying -', title);
+          const options = {
+            method: 'POST',
+            url: webhook,
+            body: {
+              content: `<@&676468893551951902> ${title} - ${link}`,
+            },
+            json: true,
+          };
+          await rp(options);
+          return await setNotified(client, topic);
         }
-
-        console.log('Notifying -', title);
-        const options = {
-          method: 'POST',
-          url: webhook,
-          body: {
-            content: `<@&676468893551951902> ${title} - ${link}`,
-          },
-          json: true,
-        };
-        await rp(options);
-        return await setNotified(client, topic);
       }
-    }
-  }));
+    }));
 
-  client.end();
+    client.end();
+  } catch (err) {
+    console.log('error');
+    console.log('Error executing', err);
+  }
   setTimeout(() => run(), process.env.INTERVAL || 1000);
 };
 
